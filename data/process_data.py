5850e420ee3c199bb04dac7dcf4c95a2c256115f
Ankur Kumar <akit9815@gmail.com>
2021-10-19 21:05:57 +0530

    added ETL code

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,55 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Takes paths of two datasets as input, merges their dataframe 
+    and returns the resulting dataframe
+    
+    '''
 
+    messages=pd.read_csv(messages_filepath)
+    categories=pd.read_csv(categories_filepath)
+    df=messages.merge(categories, on="id")
+    return df
 
 def clean_data(df):
-    pass
+    '''
+    splits the categories column into separate columns,
+    converts values to binary, and drops duplicates
+        Input
+            df: dataframe to be cleaned
+
+        Output
+            df: cleaned dataframe    
+    '''
+    categories = df.categories.str.split(";",expand=True)
+    
+    row = categories.iloc[0]
+
+    category_colnames = row.apply(lambda x:x[:-2])
+    
+    categories.columns = category_colnames
+    
+    for column in categories:
+      
+        categories[column] = categories[column].str[-1]
+
+        categories[column] = categories[column].astype("int")
 
 
-def save_data(df, database_filename):
-    pass  
+    df.drop("categories",axis=1,inplace=True)
+    df = pd.concat([df,categories],axis=1)
+    df.drop_duplicates(inplace=True)
+    return df
+    
+def save_data(df, database_filename):    
+    '''
+    saves the dataframe in a sqlite database in a table "msg_cat"
+    '''
+    engine = create_engine(f'sqlite:///{database_filename.db}')
+    df.to_sql('msg_cat', engine, index=False)
 
 
 def main():
